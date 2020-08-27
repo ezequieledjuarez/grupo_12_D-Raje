d5458b26de4e2505ac838f652126f51d9c0c3e3f
EzzeGuason <ezze.juarezz@gmail.com>
2020-08-27 19:20:01 -0300

    Adicion de carpeta controllers,nuevas rutas

---
+++ site/routes/products.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var productsController = require('../controllers/productsController')
+
+router.get('/', )
+
+router.get('/detalle/:id', )
+
+
+module.exports = router;
--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+/*Carrito de compra*/
+router.get('/shop')
 
 module.exports = router;
