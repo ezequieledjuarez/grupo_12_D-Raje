ba4d24c437ad732756aeebedd16dcdd4f98c7115
EzzeGuason <ezze.juarezz@gmail.com>
2020-09-28 20:07:31 -0300

    Fix dbUsers

--- site/data/dbUsers.js
+++ site/data/dbUsers.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 const path = require('path')
 
-module.export = JSON.parse(fs.readFileSync(path.join(__dirname,'./idUsuarios.json'),'utf-8'))
+module.export = JSON.parse(fs.readFileSync(path.join(__dirname,'./usuarios.json'),'utf-8'))
