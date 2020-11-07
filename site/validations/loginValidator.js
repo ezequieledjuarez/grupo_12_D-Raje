3e313c55a27c401ac604dcb7ad76b20831b99734
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-07 19:16:36 -0300

    Cambios en el perfil de algunos usuarios

--- site/validations/loginValidator.js
+++ site/validations/loginValidator.js
@@ -29,46 +29,4 @@ module.exports = [
             return Promise.reject('Datos incorrectos')
         })
     })
-    /* check('correo')
-    .isEmail()
-    .withMessage('Debes ingresar un email valido'),
-
-    body('correo')
-    .custom(function(value){
-        let user = dbUsers.filter(user=>{
-            return user.correo == value
-        })
-        if(user == false){
-            return false 
-        }else{
-            return true
-        }
-    })
-    .withMessage('El usuario ingresado no existe'),
-
-    check('password')
-    .isLength({
-        min:1
-    })
-    .withMessage('Ingresa tu contraseña'),
-    
-    body('password')
-    .custom(function(value, {req}){
-        let resultado = true
-        dbUsers.forEach(user=>{
-            if(user.correo == req.body.correo){
-                if(!bcrypt.compareSync(value,user.password)){
-                    resultado = false
-                }
-            }
-        })
-        if(resultado == false){
-            return false
-        }
-        else{
-            return true
-        }
-    })
-    .withMessage('Contraseña incorrecta') */
 ]
-
