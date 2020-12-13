d2e071cc1afc2327f83504a4476ef1eddfe69d37
ezequieledjuarez <ezequieledjuarez@gmail.com>
2020-12-13 20:14:42 -0300

    Imagenes en vista de productos con margen aplicado

--- site/data/structure.sql
+++ site/data/structure.sql
@@ -5,17 +5,17 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema DRajeFork
+-- Schema DRaje
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema DRajeFork
+-- Schema DRaje
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `DRajeFork` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `DRaje` DEFAULT CHARACTER SET utf8 ;
 USE `DRaje` ;
 
 -- -----------------------------------------------------
--- Table `DRajeFork`.`Users`
+-- Table `DRaje`.`Users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DRaje`.`Users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -25,35 +25,15 @@ CREATE TABLE IF NOT EXISTS `DRaje`.`Users` (
   `password` VARCHAR(128) NOT NULL,
   `categoria` VARCHAR(45) NOT NULL,
   `image` VARCHAR(128) NULL,
-  `created_at` DATE NULL,
-  `updated_at` DATE NULL,
-  `carts_id_product` INT UNSIGNED NOT NULL,
+  `createdAt` DATE NULL,
+  `updatedAt` DATE NULL,
+  `Carts_idProduct` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DRajeFork`.`Categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DRaje`.`Categories` (
-  `id` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `DRajeFork`.`States`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DRaje`.`States` (
-  `id` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `DRajeFork`.`Products`
+-- Table `DRaje`.`Products`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DRaje`.`Products` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -63,35 +43,25 @@ CREATE TABLE IF NOT EXISTS `DRaje`.`Products` (
   `precio` DECIMAL(8,2) UNSIGNED NOT NULL,
   `descuento` INT UNSIGNED NULL,
   `image` VARCHAR(128) NULL,
-  `created_at` DATE NULL,
-  `updated_at` DATE NULL,
-  `disponible` TINYINT(2) NOT NULL,
-  `id_categories` INT NOT NULL,
-  `id_states` INT NOT NULL,
-  PRIMARY KEY (`id`),
-
-    FOREIGN KEY (`id_categories`)
-    REFERENCES `DRaje`.`Categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `state_id`
-    FOREIGN KEY (`id_states`)
-    REFERENCES `DRaje`.`States` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `createdAt` DATE NULL,
+  `updatedAt` DATE NULL,
+  `estado` VARCHAR(45) NOT NULL,
+  `categoria` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DRajeFork`.`Carts`
+-- Table `DRaje`.`Carts`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DRaje`.`Carts` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_user` INT UNSIGNED NOT NULL,
+  `idUser` INT UNSIGNED NOT NULL,
   `cantidad` INT NULL,
   `total` DECIMAL(8,2) NULL,
-  PRIMARY KEY (`id`, `id_user`),
-    FOREIGN KEY (`id_user`)
+  PRIMARY KEY (`id`, `idUser`),
+  CONSTRAINT `idUser`
+    FOREIGN KEY (`idUser`)
     REFERENCES `DRaje`.`Users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -99,18 +69,20 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DRajeFork`.`ProductXCart`
+-- Table `DRaje`.`ProductXCart`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `DRaje`.`ProductXCart` (
-  `id_product` INT UNSIGNED NOT NULL,
-  `id_cart` INT UNSIGNED NOT NULL,
+  `idProduct` INT UNSIGNED NOT NULL,
+  `idCart` INT UNSIGNED NOT NULL,
   `cantidad` INT NULL,
-  PRIMARY KEY (`id_product`, `id_cart`),
-    FOREIGN KEY (`id_cart`)
+  PRIMARY KEY (`idProduct`, `idCart`),
+  CONSTRAINT `idCart`
+    FOREIGN KEY (`idCart`)
     REFERENCES `DRaje`.`Carts` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    FOREIGN KEY (`id_product`)
+  CONSTRAINT `idProduct`
+    FOREIGN KEY (`idProduct`)
     REFERENCES `DRaje`.`Products` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
